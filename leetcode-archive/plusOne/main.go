447d43b6e654343d792721c8e302b862c332ffc7
Thi Quach <amylyquach@gmail.com>
2025-10-24 16:33:47 +1100

    update plus one

--- leetcode-archive/plusOne/main.go
+++ leetcode-archive/plusOne/main.go
@@ -3,36 +3,17 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func sliceToInt(s []int) int {
-    res := 0
-    op := 1
-    for i := len(s) - 1; i >= 0; i-- {
-        res += s[i] * op
-        op *= 10
-    }
-    return res
-}
-
 func plusOne(digits []int) []int {
-	if (len(digits) == 0) {
-		return []int {}
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
 	}
-
-	value := sliceToInt(digits)
-	value++
-
-	s := strconv.Itoa(value)
-	result := make([]int, 0, len(s)) 
-
-	for _, char := range s {
-		digit, _ := strconv.Atoi(string(char))
-		result = append(result, digit)
-	}
-
-	return result
+	return append([]int{1}, digits...)
 }
 
 func main() {
