d6a3bf84bcf7122d13a069e0000f48ba9b577308
Thi Quach <amylyquach@gmail.com>
2025-10-31 13:03:37 +1100

    add Fizz Buzz -leetcode#412

---
+++ leetcode-archive/fizzBuzz/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fizzBuzz(n int) []string {
+	result := make([]string, 0)
+
+	for i:=1; i<=n; i++ {
+		if i % 15 == 0 {
+			result = append(result, "FizzBuzz")
+		} else {
+			if i % 3 == 0 || i % 5 == 0 {
+				if i % 3 == 0 {
+					result = append(result, "Fizz")
+				} else if i % 5 == 0 {
+					result = append(result, "Buzz")
+				} else {
+					result = append(result, strconv.Itoa(i))
+				}
+			} else {
+				result = append(result, strconv.Itoa(i))
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	n := 15
+	fmt.Println("FizzBuzz ", fizzBuzz(n))
+}
